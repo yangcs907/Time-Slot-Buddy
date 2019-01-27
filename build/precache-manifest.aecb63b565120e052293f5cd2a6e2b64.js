47908fa977e3972d6e756ae2b7e93d3ed7cc0126
yangcs907 <>
2019-01-27 01:15:13 -0500

    added build

---
+++ build/precache-manifest.aecb63b565120e052293f5cd2a6e2b64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2cdd4bfafb91fef11c268b1b3bbbed09",
+    "url": "/static/media/background.2cdd4bfa.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b4c4fe9d3afc6aa62cc",
+    "url": "/static/js/main.6b4c4fe9.chunk.js"
+  },
+  {
+    "revision": "8662d0d2e3adfdf31ee8",
+    "url": "/static/js/1.8662d0d2.chunk.js"
+  },
+  {
+    "revision": "6b4c4fe9d3afc6aa62cc",
+    "url": "/static/css/main.6459893b.chunk.css"
+  },
+  {
+    "revision": "92eac0528b90a119664ae777635a2326",
+    "url": "/index.html"
+  }
+];
